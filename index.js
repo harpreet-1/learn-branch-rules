f5d2a7bc740b77cc96f415f6150dac344628da7b
harpreet-gtet <harpreet.singh@narayanagroup.com>
2024-04-22 17:17:36 +0530

    change

--- index.js
+++ index.js
@@ -6,5 +6,6 @@ for(let i=0;i<a.length;i++){
     console.log("abasds",a[i]);
     console.log("changes from branch2")
     console.log("changes from branch1 ")
+    console.log("change froma");
     log
 }
