247e462d5fa67ca5ce54008df50bf31b41af799b
harpreet-gtet <harpreet.singh@narayanagroup.com>
2024-04-22 16:46:02 +0530

    Revert "changes in 7-2"
    This reverts commit 2c7a71651cbbf9f56b0d3f73d1ae7ddfa930a71e.

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ let a="helo"
 console.log("hello")
 for(let i=0;i<a.length;i++){
     console.log("abasds",a[i]);
-    console.log("changes from branch2")
     console.log("changes from branch1 ")
     console.log("changes from branch2")
     console.log("changes from branch1 ")
